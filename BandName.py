d654693ed4d3515a23977435728b3881ac213860
petiteloup <112962449+petiteloup@users.noreply.github.com>
2023-02-03 13:59:57 -0500

    Initial commit

---
+++ BandName.py
@@ -0,0 +1,6 @@
+# This is a band naming generator. Day 1 of a Python course
+intro = "Welcome to the Band Name Generator"
+city =input("What is the name of the city you live in?")
+pet =input("What is the name of your first pet")
+print("Your band name could be " + city +" " + pet)
+
